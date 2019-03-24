c991c086dfa87f8ace3a6e452dd266112dd5182c
António <pg11024@gmail.com>
2019-03-24 01:39:46 +0000

    First working crosswords

--- words/src/Lib.hs
+++ words/src/Lib.hs
@@ -1,6 +1,78 @@
 module Lib
-    ( someFunc
+    ( grid
+    , languages
+    , formatGrid
+    , outputGrid
+    , findWord
+    , findWordInLine
+    , findWords
+    , skew
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List (isInfixOf, transpose)
+import Data.Maybe (catMaybes)
+
+type Grid = [String]
+
+outputGrid :: Grid -> IO ()
+outputGrid = putStrLn . formatGrid
+
+formatGrid :: Grid -> String
+formatGrid = unlines
+
+getLines :: Grid -> [String]
+getLines grid =
+  let horizontal = grid
+      vertical = transpose grid
+      diagonal1 = diagnolize grid
+      diagonal2 = diagnolize (map reverse grid)
+      lines = horizontal ++ vertical ++ diagonal1 ++ diagonal2
+  in lines ++ map reverse lines
+
+diagnolize :: Grid -> Grid
+diagnolize = transpose . skew
+
+skew :: Grid -> Grid
+skew [] = []
+skew (l:ls) = l : skew (map indent ls)
+  where indent line = '_' : line
+
+findWord :: Grid -> String -> Maybe String
+findWord grid word =
+  let lines = getLines grid
+      found = or $ map (findWordInLine word) lines
+  in if found then Just word else Nothing
+
+findWords :: Grid -> [String] -> [String]
+findWords grid = catMaybes . map (findWord grid)
+
+findWordInLine :: String -> String -> Bool
+findWordInLine = isInfixOf
+
+grid :: Grid
+grid = [ "__C________R___"
+       , "__SI________U__"
+       , "__HASKELL____B_"
+       , "__A__A_____S__Y"
+       , "__R___B___C____"
+       , "__PHP____H_____"
+       , "____S_LREP_____"
+       , "____I__M_Y__L__"
+       , "____L_E__T_O___"
+       , "_________HB____"
+       , "_________O_____"
+       , "________CN_____"
+       ]
+
+languages :: [String]
+languages = [ "BASIC"
+            , "COBOL"
+            , "CSHARP"
+            , "HASKELL"
+            , "LISP"
+            , "PERL"
+            , "PHP"
+            , "PYTHON"
+            , "RUBY"
+            , "SCHEME"
+            ]
